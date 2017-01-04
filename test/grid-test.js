d0dbdfdb00c8626464f01cf51aa124e005fb3be5
Kyle Zucker <zkyle12@gmail.com>
2017-01-04 14:37:21 -0700

    add grid test

--- test/grid-test.js
+++ test/grid-test.js
@@ -1,8 +1,14 @@
 var assert = require('chai').assert
-var Grid = require('../lib/grid.js');
+var genGrid = require('../lib/grid.js');
 
 describe('grid stuff', function () {
-  it('')
-
+  it('generates a 15 by 10 grid by default', function () {
+    grid = genGrid()
+    console.log(grid);
+    //grid[x][y]
+    assert.equal(grid.length, 15)
+    assert.equal(grid[0].length, 10)
+  })
+  
 
 })
