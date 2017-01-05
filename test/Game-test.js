ccbc745bd557ca0ac8fcf4082d9e5eff89a7ec8b
Kyle Zucker <zkyle12@gmail.com>
2017-01-04 21:03:12 -0700

    clean up Game-test, add new test for returning player object

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,6 +1,15 @@
 var assert = require('chai').assert
 var Game = require('../lib/Game.js');
+var Player = require('../lib/Player.js')
 var genGrid = require('../lib/grid.js');
+
+function returnFake(x,y) {
+  var game = new Game()
+  game.grid = genGrid()
+  var s = new Player('Steve', x, y, game)
+  s.game.grid[s.x][s.y] = s
+  return game
+}
 
 // Game:
 //
@@ -14,48 +23,53 @@ var genGrid = require('../lib/grid.js');
 describe('Game Object Stuff', function () {
 
   it('game is an object', function () {
-    game = new Game()
+    game = returnFake(0,0)
     assert.isObject(game);
   });
 
   it('game saves the current level and lives', function() {
-    game = new Game()
+    game = returnFake(0,0)
     assert.equal(game.currentLevel, 0)
     assert.equal(game.lives, 3)
   })
 
   it('has a reset function. Reset reloads the level and restarts the timer', function() {
-    game = new Game()
+    game = returnFake(0,0)
     assert.isFunction(game.reset)
   })
 
   it('has a restart function. Restart goes back to level 1 and reset lives to 0', function() {
-    game = new Game()
+    game = returnFake(0,0)
     assert.isFunction(game.restart)
     assert.equal(game.currentLevel, 0)
     assert.equal(game.lives, 3)
   })
 
   it('can increment the level', function() {
-    game = new Game()
+    game = returnFake(0,0)
     assert.isFunction(game.nextLevel)
     game.nextLevel()
     assert.equal(game.currentLevel, 1)
   })
 
   it('can decrement the lives', function() {
-    game = new Game()
+    game = returnFake(0,0)
     assert.isFunction(game.loseLife)
     game.loseLife()
     assert.equal(game.lives, 2)
   })
 
   it('change to game-over when lives = 0. game.currentLevel = "game-over"', function() {
-    game = new Game()
+    game = returnFake(0,0)
     game.lives = 1
     assert.equal(game.lives, 1)
     game.loseLife()
     assert.equal(game.lives, 'game-over')
+  })
+
+  it('can return the current Player object', function() {
+    game = returnFake(0,0)
+    assert.equal(game.player.name, 'Steve')
   })
 
   it('manage the grid level')
